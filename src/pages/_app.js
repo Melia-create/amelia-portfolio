0437dd340afee3cd29e21fb7fb130bf487ed3fa7
Amelia <amelia.riding@sofology.co.uk>
2023-05-04 14:42:16 +0100

    Added About section and other styling bits

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,9 +1,5 @@
-import "bootstrap/dist/css/bootstrap.min.css";
-import { useEffect } from "react";
+import "../styles/globals.css";
 
 export default function App({ Component, pageProps }) {
-  useEffect(() => {
-    import("bootstrap/dist/js/bootstrap");
-  }, []);
   return <Component {...pageProps} />;
 }
--- src/pages/index.js
+++ src/pages/index.js
@@ -1,9 +1,12 @@
 import Navigation from "@/components/Navbar/Navigation";
+import About from "@/components/About/About";
+import Navigation from "@/components/Navbar/Navigation";
 
 export default function Home() {
   return (
     <>
       <Navigation />
+      <About />
     </>
   );
 }
