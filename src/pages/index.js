2429e4d154ce15644f64cae10d51aa6c48dbc3d1
Amelia <amelia.riding@sofology.co.uk>
2023-05-17 11:40:22 +0100

    Added contact form

--- src/pages/index.js
+++ src/pages/index.js
@@ -3,6 +3,7 @@ import Navigation from "@/components/Navbar/Navigation";
 import Banner from "@/components/Banner/Banner";
 import Skills from "@/components/Skills/Skills";
 import Portfolio from "@/components/Portfolio/Portfolio";
+import Contact from "@/components/Contact/Contact";
 
 export default function Home() {
   return (
@@ -12,6 +13,7 @@ export default function Home() {
       <About />
       <Skills />
       <Portfolio />
+      <Contact />
     </>
   );
 }
